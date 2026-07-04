e2750f95a4db2e1c769dfa9992fc59738bfcc57e
Priyanshi Singh <priyanshi2005.18@gmail.com>
2026-07-04 23:32:22 +0530

    Add React state and upload button functionality

--- frontend/src/components/Upload.jsx
+++ frontend/src/components/Upload.jsx
@@ -1,11 +1,26 @@
+import { useState } from "react";
+
 function Upload() {
+
+  const [selectedFile, setSelectedFile] = useState(null);
+  const handleUpload = () => {
+    alert("Upload button clicked!");
+  };
+
   return (
     <div>
       <h2>Upload PDF</h2>
 
-      <input type="file" />
+      <input
+        type="file"
+        onChange={(e) => setSelectedFile(e.target.files[0])}
+      />
 
-      <button>Upload</button>
+      <button onClick={handleUpload}>
+  Upload
+</button>
+
+      <p>{selectedFile?.name}</p>
     </div>
   );
 }
